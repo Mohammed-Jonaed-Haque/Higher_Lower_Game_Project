5dcb41cf0ec7a50010d418c0f8b958a1156ab793
Mohammed-Jonaed-Haque <jonaedhaque718@gmail.com>
2026-03-23 15:32:05 +1100

    updated

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import art
 
 
 def format_data(account):
+    """formating the vales in the dictionary, to show in a single line"""
     account_name = account['name']
     account_description = account['description']
     account_country = account['country']
@@ -11,47 +12,52 @@ def format_data(account):
 
 
 def compare_data(user_guess, person1, person2):
+    """compare the follower count of the two person"""
     if person1 > person2:
         return user_guess == "a"
     else:
         return user_guess == "b"
 
 
-print(art.logo)
-game_continue = True
-score = 0
-b_question = random.choice(game_data.data)
-
-
-while game_continue:
-    a_question = b_question
+def game():
+    """main functionalities of the game"""
+    print(art.logo)
+    game_continue = True
+    score = 0
     b_question = random.choice(game_data.data)
 
 
-    if a_question == b_question:
+    while game_continue:
+        a_question = b_question
         b_question = random.choice(game_data.data)
 
-    print(f"Compare A: {format_data(a_question)}")
-    print (art.vs)
-    print(f"Compare B: {format_data(b_question)}")
+
+        if a_question == b_question:
+            b_question = random.choice(game_data.data)
+
+        print(f"Compare A: {format_data(a_question)}")
+        print (art.vs)
+        print(f"Compare B: {format_data(b_question)}")
 
 
-    a_follower_count = a_question['follower_count']
-    b_follower_count = b_question['follower_count']
+        a_follower_count = a_question['follower_count']
+        b_follower_count = b_question['follower_count']
 
 
 
-    user_choice = input("Who has more followers? a/b").lower()
+        user_choice = input("Who has more followers? a/b").lower()
 
 
-    check_answer = compare_data(user_choice, a_follower_count, b_follower_count)
+        check_answer = compare_data(user_choice, a_follower_count, b_follower_count)
 
 
-    if check_answer:
-        score += 1
-        print("\n" * 100)
-        print(art.logo)
-        print(f"Well done!! Your current score is: {score}")
-    else:
-        print(f"Wrong answer!! Your current score is: {score}")
-        game_continue = False
+        if check_answer:
+            score += 1
+            print("\n" * 100)
+            print(art.logo)
+            print(f"Well done!! Your current score is: {score}")
+        else:
+            print(f"Wrong answer!! Your current score is: {score}")
+            game_continue = False
+
+game()
